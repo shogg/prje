2943461d7049c5b730fccea19b2876fda9252d25
hel <hel@ceres>
2012-12-21 17:20:56 +0100

    e0022

---
+++ e0022.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"strings"
+	"sort"
+)
+
+func main() {
+
+	resp, err := http.Get("http://projecteuler.net/project/names.txt")
+	if err != nil { panic(err) }
+
+	defer resp.Body.Close()
+	text, err := ioutil.ReadAll(resp.Body)
+	if err != nil { panic(err) }
+
+	N := names(string(text))
+	sort.Strings(N)
+
+	sum := 0
+	for i, n := range N {
+		sum += (i+1) * score(n)
+	}
+
+	println(sum)
+}
+
+func names(s string) []string {
+
+	n := strings.Split(s, ",")
+	for i := 0; i < len(n); i++ {
+		n[i] = strings.Trim(n[i], "\"\t ")
+	}
+
+	return n
+}
+
+func score(name string) int {
+
+	s := 0
+	for _, n := range name {
+		s += int(n) - int('A') + 1
+	}
+
+	return s
+}
