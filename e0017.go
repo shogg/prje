2c02db44c2ded387f9cb02ff8941f5bb60c98c11
shogg <lars.heyden@gmail.com>
2012-12-26 22:56:04 +0100

    e0017 - incomplete

---
+++ e0017.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+	"fmt"
+)
+
+var (
+	digits = []string {
+		"", "one", "two", "three", "four",
+		"five", "six", "seven", "eight", "nine" }
+	teens = []string {
+		"ten", "eleven", "twelve", "thirteen", "fourteen",
+		"fifteen", "sixteen", "seventeen", "eighteen", "nineteen" }
+	tys = []string {
+		"", "", "twenty", "thirty", "forty",
+		"fifty", "sixty", "seventy", "eighty", "ninety" }
+	magnitude = []string { "", "", "hundred", "thousand" }
+)
+
+func main() {
+
+	chars := 0
+	for n := 1; n <= 1000; n++ {
+		d := split(n)
+		w := written(d)
+		chars += len(w)
+	}
+
+	println(chars)
+}
+
+func split(n int) []int {
+	str := strings.Split(strconv.Itoa(n), "")
+	d := make([]int, len(str))
+	for i, s := range str {
+		d[len(d)-i-1], _ = strconv.Atoi(s)
+	}
+
+	fmt.Println(d)
+
+	return d
+}
+
+func written(d []int) string {
+
+	s := ""
+	for i := 0; i < len(d); i++ {
+		if d[i] == 0 { continue }
+		if len(d) >= 2 && i == 0 && d[1] == 1 { continue }
+		if i == 1 && d[i] == 1 { s += teens[d[0]]; break }
+		if i == 1 { s += tys[d[i]]; continue }
+		s += digits[d[i]] + magnitude[i]
+	}
+
+	if len(d) > 2 && (d[0] != 0 || d[1] != 0) {
+		s += "and"
+	}
+
+	println(s)
+
+	return s
+}
