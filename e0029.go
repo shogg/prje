0d73bda1b6b46d770ec87ae088ecfdd20779993f
shogg <lars.heyden@gmail.com>
2013-01-02 21:23:52 +0100

    e0029

--- e0029.go
+++ e0029.go
@@ -1,49 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-const (
-	N = 10
-)
-
-var (
-	matrix [N+1][N+1]bool
-)
-
-type primefactor struct {
-	prime, exp int
-}
+const N = 100
 
 func main() {
 
-	// 2^4 == 4^2 (2^(2*2))
-	// 2^6 (2^(2*3)) == 8^2 (2^3^2) == 4^3 (2^2^3)
-	// 2^8 == 4^4 (2^(2*4)) == 16^2 (2^(4*2))
-	// 2^24 == (2^(3*2^3)) (2^(3*8)) (2^(4*6)) (2^(2*12))
-	// 3^4 (3^(2*2)) == 9^2 (3^2^2)
-	// 3^6 (3^(2*3)) == 27^2 (3^3^2) == 9^3 (3^2^3)
-	// 4^6 (2^2^(2*3)) == 8^4 (2^3^(2*2)) (2^(2*2*3)) 
-	// 5^4 == 25^2 (5^(2*2))
-	// 5^6 == 25^4 (5^(2*3)) == 125^2 (5^(3*2))
+	var matrix [N+1][N+1]bool
 
 	for a := 2; a <= N; a++ {
-		factorsA := primefactors(a)
+		prime, exp := same(primefactors(a))
 		for b := 2; b <= N; b++ {
-			for _, fb := range factors(b) {
-				a1 := pow(a, fb)
-				b1 := b/fb
-				if a1 <= N {
+			a0, b0 := a, b
+			if exp >= 2 {
+				a0, b0 = prime, b * exp
+			}
+			for _, f := range factors(b0) {
+				a1 := pow(a0, f)
+				b1 := b0 / f
+				if a1 > a && a1 <= N && b1 <= N {
 					matrix[a1][b1] = true
-				}
-				if len(factorsA) == 1 && factorsA[0].exp > 1 && factorsA[0].exp < fb {
-					a2 := pow(factorsA[0].prime, fb)
-					b2 := b/fb*factorsA[0].exp
-					if a2 <= N && b2 <= N {
-						matrix[a2][b2] = true
-					}
 				}
 			}
 		}
@@ -52,32 +26,28 @@ func main() {
 	count := 0
 	for a := 2; a <= N; a++ {
 		for b := 2; b <= N; b++ {
-			if matrix[a][b] == false {
+			if ! matrix[a][b] {
 				count++
-			} else {
-				fmt.Printf("%d^%d ", a, b)
 			}
 		}
-	}
-	fmt.Println()
-
-	for a := 2; a <= N; a++ {
-		fmt.Printf("%3d ", a)
-		for b := 2; b <= N; b++ {
-			if matrix[a][b] == true {
-				fmt.Print("T "); continue
-			} else {
-				fmt.Print(". ")
-			}
-		}
-		fmt.Println()
 	}
 
 	println(count)
-	println(len(distinctPowers(N)))
 }
 
 func factors(n int) []int {
+
+	var result []int
+	for f := 2; f < n; f++ {
+		if n % f == 0 {
+			result = append(result, f)
+		}
+	}
+
+	return result
+}
+
+func primefactors(n int) []int {
 
 	var result []int
 	for f := 2; f <= n; f++ {
@@ -90,22 +60,14 @@ func factors(n int) []int {
 	return result
 }
 
-func primefactors(n int) []primefactor {
+func same(f []int) (prime, exp int) {
 
-	var result []primefactor
-	for f := 2; f <= n; f++ {
-		pf := primefactor { f, 0 }
-		for n % f == 0 {
-			pf.exp++
-			n /= f
-		}
-
-		if pf.exp > 0 {
-			result = append(result, pf)
-		}
+	cmp := f[0]
+	for _, p := range f {
+		if p != cmp { return 0, 0 }
 	}
 
-	return result
+	return cmp, len(f)
 }
 
 func pow(n, e int) int {
@@ -117,38 +79,4 @@ func pow(n, e int) int {
 	}
 
 	return p
-}
-
-func powExact(n, e int) int {
-	p := 1
-	for i := 0; i < e; i++ {
-		p *= n
-	}
-
-	return p
-}
-
-func distinctPowers(n int) []int {
-
-	var powers []int
-	for a := 2; a <= n; a++ {
-		for b := 2; b <= n; b++ {
-			powers = append(powers, powExact(a, b))
-		}
-	}
-
-	sort.Ints(powers)
-
-	var distinct []int
-	last := 0
-	for _, p := range powers {
-		if p != last {
-			distinct = append(distinct, p)
-			last = p
-		} else {
-			println(p)
-		}
-	}
-
-	return distinct
 }
