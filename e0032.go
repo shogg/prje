96e88e55fc99fb4a7f1892700d1b60a58ed38a4a
hel <hel@ceres>
2013-01-03 17:45:38 +0100

    e0032

---
+++ e0032.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"sort"
+)
+
+func main() {
+
+	var numbers []int
+	for a := 1; a < 9998; a++ {
+		for b := a + 1; b < 9999; b++ {
+			d := append(append(digits(a), digits(b)...), digits(a*b)...)
+			if len(d) > 9 { break }
+			if pandigital(9, d) {
+				numbers = append(numbers, a*b)
+			}
+		}
+	}
+
+	sort.Ints(numbers)
+
+	var uniq []int
+	last := 0
+	for _, n := range numbers {
+		if n == last { continue }
+		uniq = append(uniq, n)
+		last = n
+	}
+
+	sum := 0
+	for _, n := range uniq {
+		sum += n
+	}
+
+	println(sum)
+}
+
+func digits(n int) []int {
+	var d []int
+	for n >= 10 {
+		d = append(d, n % 10)
+		n /= 10
+	}
+	d = append(d, n)
+
+	return d
+}
+
+func pandigital(n int, digits []int) bool {
+	if len(digits) != n { return false }
+
+	sort.Ints(digits)
+
+	for i, d := range digits {
+		if d != i + 1 { return false }
+	}
+
+	return true
+}
